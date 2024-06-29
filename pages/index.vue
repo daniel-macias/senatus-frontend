41355d1150d1c675a2d66afd9739a3e3869589aa
daniel-macias <josedanielmr@gmail.com>
2024-06-28 22:41:27 -0600

    new file org

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container mx-auto p-4">
+    <h1 class="text-4xl font-bold mb-4">Welcome to Your Nuxt.js App</h1>
+    <p class="mb-4">This is the home page of your Nuxt.js application.</p>
+    <nuxt-link to="/about" class="text-blue-500 hover:underline">Go to About Page</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+
+<style scoped>
+.container {
+  max-width: 800px;
+  margin: 0 auto;
+}
+</style>
---
+++ pages/vote/[id]/edit.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <!-- Your template code here -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    // Your script code here
+  }
+  </script>
+  
+  <style scoped>
+  /* Your style code here */
+  </style>
